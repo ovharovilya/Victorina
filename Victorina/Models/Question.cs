2249c3e1dce0c1c283ad1ab6e0b0d338563d3448
ovharovilya <ovharovilya@gmail.com>
2026-05-07 20:58:18 +0300

    first commit

---
+++ Victorina/Models/Question.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Victorina.Models
+{
+    public class Question
+    {
+        public string Text { get; set; }
+        public List<string> Options { get; set; }
+        public int CorrectIndex {  get; set; }
+        public Question(string text, List<string> options, int correctIndex)
+        {
+            Text = text;
+            Options = options;
+            CorrectIndex = correctIndex;
+
+        }
+
+
+    }
+}
---
+++ Victorina/Models/QuizData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Victorina.Models
+{
+    public static class QuizData
+    {
+        public static List<Question> GetQuestions()
+        {
+            return new List<Question>()
+            {
+                new Question("Какой тип данных используется для целых чисел?", new List<string> {"string", "bool", "int", "double"}, 2),
+                new Question("Какой тип данных используется для строк?", new List<string> {"string", "bool", "int", "double"}, 0),
+                new Question("Какой модификатор доступа делает поля и методы видимыми для всех классов?", new List<string> {"private", "public", "protected", "bool"}, 1),
+                new Question("Какой модификатор доступа делает поля и методы невидимыми для всех классов?", new List<string> {"private", "public", "protected", "int"}, 0),
+                new Question("Какой тип данных используется для целых чисел?", new List<string> {"string", "bool", "int", "double"}, 2),
+                new Question("Какой тип данных используется для строк?", new List<string> {"string", "bool", "int", "double"}, 0),
+                new Question("Какой модификатор доступа делает поля и методы видимыми для всех классов?", new List<string> {"private", "public", "protected", "bool"}, 1),
+                new Question("Какой модификатор доступа делает поля и методы невидимыми для всех классов?", new List<string> {"private", "public", "protected", "int"}, 0),
+                new Question("Какой тип данных используется для целых чисел?", new List<string> {"string", "bool", "int", "double"}, 2),
+                new Question("Какой тип данных используется для строк?", new List<string> {"string", "bool", "int", "double"}, 0),
+            };
+        }
+    }
+}
